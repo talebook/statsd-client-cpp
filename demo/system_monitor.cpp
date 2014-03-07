f2f546c7fe2510b325e59b6e189aa723398a6b63
Rex <talebook@foxmail.com>
2014-03-07 13:14:41 +0800

    rewrite system_monitor, monitor all net interface

--- demo/system_monitor.cpp
+++ demo/system_monitor.cpp
@@ -1,17 +1,22 @@
+
 #include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
 #include <string.h>
+#include <netdb.h>
 #include <sys/sysinfo.h>
 
 #include <sys/ioctl.h>
 #include <netinet/in.h>
 #include <net/if.h>
+#include <string>
+#include <vector>
 
 #include "statsd_client.h"
-using namespace statsd;
+
+using namespace std;
 
 static int running = 1;
 
@@ -20,51 +25,78 @@ void sigterm(int sig)
     running = 0;
 }
 
-#define MAX_LINE_LEN 200
-#define PKT_LEN 1400
+string localhost() {
+    struct addrinfo hints, *info, *p;
+    string hostname;
+    hostname.resize(1024);
+    gethostname((char*)hostname.data(), 1023);
 
-int find_my_mac(const char *iface, char *mac)
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC; /*either IPV4 or IPV6*/
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_CANONNAME;
+
+    if ( getaddrinfo(hostname.c_str(), "http", &hints, &info) == 0) {
+        for(p = info; p != NULL; p = p->ai_next) {
+            hostname = p->ai_canonname;
+        }
+        freeaddrinfo(info);
+    }
+
+    string::size_type pos = hostname.find(".");
+    while ( pos != string::npos )
+    {
+        hostname[pos] = '_';
+        pos = hostname.find(".", pos);
+    }
+    return hostname;
+}
+
+vector<string>& StringSplitTrim(const string& sData,
+        const string& sDelim, vector<string>& vItems)
 {
-    struct ifreq ifr;
-    memset(&ifr, 0, sizeof(struct ifreq));
-    strncpy(ifr.ifr_name, iface, IFNAMSIZ);
-    int s = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
-    if (s == -1) {
-        perror("socket");
-        return -1;
+    vItems.clear();
+
+    string::size_type bpos = 0;
+    string::size_type epos = 0;
+    string::size_type nlen = sDelim.size();
+
+    while(sData.substr(epos,nlen) == sDelim)
+    {
+        epos += nlen;
+    }
+    bpos = epos;
+
+    while ((epos=sData.find(sDelim, epos)) != string::npos)
+    {
+        vItems.push_back(sData.substr(bpos, epos-bpos));
+        epos += nlen;
+        while(sData.substr(epos,nlen) == sDelim)
+        {
+            epos += nlen;
+        }
+        bpos = epos;
     }
 
-    if ( ioctl(s, SIOCGIFHWADDR, &ifr) == -1 ) {
-        perror("ioctl(SIOCGIFHWADDR)");
-        close(s);
-        return -1;
+    if(bpos != sData.size())
+    {
+        vItems.push_back(sData.substr(bpos, sData.size()-bpos));
     }
-    unsigned char macaddr[6];
-    memcpy(macaddr, ifr.ifr_hwaddr.sa_data, 6);
-    sprintf(mac, "%.2X%.2X%.2X%.2X%.2X%.2X",
-            macaddr[0], macaddr[1], macaddr[2],
-            macaddr[3], macaddr[4], macaddr[5]);
-    close(s);
-    return 0;
+    return vItems;
 }
 
 int main(int argc, char *argv[])
 {
     FILE *net, *stat;
     struct sysinfo si;
-    char line[MAX_LINE_LEN], ns[16];
-    char *lasts, *iface, *p;
+    char line[256];
     unsigned int user, nice, sys, idle, total, busy, old_total=0, old_busy=0;
-    int i;
 
     if (argc != 3) {
-        printf("Usage: %s host interface\n\nEg: %s 127.0.0.1 eth0", argv[0], argv[0]);
+        printf("Usage: %s host port\n"
+                "Eg: %s 127.0.0.1 8125\n",
+                argv[0], argv[0]);
         exit(1);
-    }
-    iface = argv[2];
-    strcpy(ns, "host.");
-    if (find_my_mac(iface, ns + 5)!=0) {
-        fprintf(stderr, "Cannot find mac address\n");
     }
 
     signal(SIGHUP, SIG_IGN);
@@ -82,43 +114,43 @@ int main(int argc, char *argv[])
         exit(-1);
     }
 
-    StatsdClient client = StatsdClient(argv[1], 8125, ns);
+    string ns = string("host.") + localhost() + ".";
+    statsd::StatsdClient client(argv[1], atoi(argv[2]), ns);
 
     daemon(0,0);
+    printf("running in background.\n");
 
     while(running) {
         sysinfo(&si);
-        client.send("load", 100*si.loads[0]/0x10000, "g", 1.0);
-        client.send("freemem", si.freeram/1024, "g", 1.0);
-        client.send("procs", si.procs, "g", 1.0);
-        client.send("uptime", si.uptime, "c", 1.0);
+        client.gauge("system.load", 100*si.loads[0]/0x10000);
+        client.gauge("system.freemem", si.freeram/1024);
+        client.gauge("system.procs", si.procs);
+        client.count("system.uptime", si.uptime);
 
         rewind(net);
+        vector<string> items;
         while(!feof(net)) {
-            fgets(line, MAX_LINE_LEN, net);
-            if (!strstr(line, iface)) {
-                continue;
-            }
-            i = 0;
-            for (i = 0, p = strtok_r(line, " ", &lasts); p;
-                p = strtok_r(NULL, " ", &lasts), i++) {
-                if (i == 1) {
-                    client.send("if-rx", atoi(p), "c", 1.0);
-                } else if (i == 9) {
-                    client.send("if-tx", atoi(p), "c", 1.0);
-                    break;
-                }
-            }
+            fgets(line, sizeof(line), net);
+            StringSplitTrim(line, " ", items);
+
+            if ( items.size() < 17 ) continue;
+            if ( items[0].find(":") == string::npos ) continue;
+
+            string netface = items[0].erase( items[0].find(":") );
+            client.count( netface+".receive.bytes", atoi(items[1].c_str()) );
+            client.count( netface+".receive.packets", atoi(items[2].c_str()) );
+            client.count( netface+".transmit.bytes", atoi(items[9].c_str()) );
+            client.count( netface+".transmit.bytes", atoi(items[10].c_str()) );
         }
 
         /* rewind doesn't do the trick for /proc/stat */
         freopen("/proc/stat", "r", stat);
-        fgets(line, MAX_LINE_LEN, stat);
+        fgets(line, sizeof(line), stat);
         sscanf(line, "cpu  %u %u %u %u", &user, &nice, &sys, &idle);
         total = user + sys + idle;
         busy = user + sys;
 
-        client.send("cpu", 100 * (busy - old_busy)/(total - old_total), "g", 1.0);
+        client.send("system.cpu", 100 * (busy - old_busy)/(total - old_total), "g", 1.0);
 
         old_total = total;
         old_busy = busy;
--- demo/test_client.cpp
+++ demo/test_client.cpp
@@ -2,14 +2,13 @@
 #include <iostream>
 #include <unistd.h>
 #include "statsd_client.h"
-using namespace statsd;
 
 int main(void)
 {
     std::cout << "running..." << std::endl;
-    
-    StatsdClient client("127.0.0.1", 8125);
-    StatsdClient client2("127.0.0.1", 8125, "myproject.abx.");
+
+    statsd::StatsdClient client;
+    statsd::StatsdClient client2("127.0.0.1", 8125, "myproject.abx.");
 
     client.count("count1", 123, 1.0);
     client.count("count2", 125, 1.0);
