dd5df80744c3ba30c29ad3ef79e2bbfa056d7b03
Deng Liu <dengl@twitter.com>
2015-10-26 17:18:28 -0700

    Add batching option to statsd-client-cpp
    This change is to improve the efficiency of communication between the client and
    stated daemon.
    The main thread will be just generating stats (in the format of character string)
    in memory and a worker threads (a.k.a flusher) will be running in the background
    to asynchronously sending batched stats to stated daemon via UDP package.
    The flusher wake up every second and send all accumulated stats to the statsd
    daemon.

--- demo/test_client.cpp
+++ demo/test_client.cpp
@@ -8,19 +8,20 @@ int main(void)
     std::cout << "running..." << std::endl;
 
     statsd::StatsdClient client;
-    statsd::StatsdClient client2("127.0.0.1", 8125, "myproject.abx.");
+    statsd::StatsdClient client2("127.0.0.1", 8125, "myproject.abx.", false);
 
     client.count("count1", 123, 1.0);
     client.count("count2", 125, 1.0);
     client.gauge("speed", 10);
-    client2.timing("request", 2400);
+    int i;
+    for (i=0; i<1000; i++)
+        client2.timing("request", i);
     sleep(1);
     client.inc("count1", 1.0);
     client2.dec("count2", 1.0);
-    int i;
-    for(i=0; i<10; i++) {
-        client2.count("count3", i, 0.8);
-    }
+//    for(i=0; i<1000; i++) {
+//        client2.count("count3", i, 0.8);
+//    }
 
     std::cout << "done" << std::endl;
     return 0;
